5933ffc758b0e80d6d237fac212df2809dd0fc58
Michael Dales <mwd24@cam.ac.uk>
2025-12-22 11:52:49 +0000

    Update tests

--- test/test_raybook.ml
+++ test/test_raybook.ml
@@ -12,19 +12,15 @@ let test_create_tuple _ =
 
 let test_create_point _ =
   let res : Tuple.t = Tuple.point 4.3 (-4.2) 3.1 in
-  assert_equal 4.3 (Tuple.x res);
-  assert_equal (-4.2) (Tuple.y res);
-  assert_equal 3.1 (Tuple.z res);
-  assert_equal 1.0 (Tuple.w res);
+  let expected = Tuple.v 4.3 (-4.2) 3.1 1.0 in
+  assert_equal expected res;
   assert_bool "is point" (Tuple.is_point res);
   assert_bool "is vector" (not (Tuple.is_vector res))
 
 let test_create_vector _ =
   let res : Tuple.t = Tuple.vector 4.3 (-4.2) 3.1 in
-  assert_equal 4.3 (Tuple.x res);
-  assert_equal (-4.2) (Tuple.y res);
-  assert_equal 3.1 (Tuple.z res);
-  assert_equal 0.0 (Tuple.w res);
+  let expected = Tuple.v 4.3 (-4.2) 3.1 0.0 in
+  assert_equal expected res;
   assert_bool "is point" (not (Tuple.is_point res));
   assert_bool "is vector" (Tuple.is_vector res)
 
